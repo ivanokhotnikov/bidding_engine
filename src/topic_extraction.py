db0b9e7eac03f3b624c0e607dd0f60d560595214
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2023-03-21 15:38:02 +0000

    renamed, features added, granularity in models added

--- src/topic_extraction.py
+++ src/topic_extraction.py
@@ -14,7 +14,9 @@ from umap import UMAP
 from wordcloud import WordCloud
 
 
-def visualize_topics(kwds: list, topic_model: BERTopic) -> None:
+def visualize_topics(kwds: list,
+                     topic_model: BERTopic,
+                     ext: str = 'png') -> None:
     """
     The visualize_topics function takes a list of keywords and a trained topic model as input.
     It then creates word clouds for each topic in the model, using the keywords that are assigned to that topic.
@@ -27,6 +29,8 @@ def visualize_topics(kwds: list, topic_model: BERTopic) -> None:
             Specify the keywords to be used for the topic model
         topic_model: BERTopic
             Pass the model to the function
+        ext: str
+            Image file extension
 
     Returns
     -------
@@ -45,13 +49,12 @@ def visualize_topics(kwds: list, topic_model: BERTopic) -> None:
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')
         plt.title(name)
-        plt.savefig(os.path.join(os.environ['IMAGES_PATH'], f'{name}.pdf'),
+        plt.savefig(os.path.join(os.environ['IMAGES_PATH'], f'{name}.{ext}'),
                     bbox_inches='tight')
-        plt.show()
     topic_model.visualize_hierarchy().write_image(
-        os.path.join(os.environ['IMAGES_PATH'], 'hierarchy.pdf'))
+        os.path.join(os.environ['IMAGES_PATH'], f'hierarchy.{ext}'))
     topic_model.visualize_barchart().write_image(
-        os.path.join(os.environ['IMAGES_PATH'], 'barchart.pdf'))
+        os.path.join(os.environ['IMAGES_PATH'], f'barchart.{ext}'))
     embeddings = topic_model.embedding_model.embedding_model.encode(kwds)
     reduced_embeddings = UMAP(n_neighbors=15,
                               n_components=2,
@@ -59,14 +62,18 @@ def visualize_topics(kwds: list, topic_model: BERTopic) -> None:
                               metric='cosine').fit_transform(embeddings)
     topic_model.visualize_documents(
         kwds, reduced_embeddings=reduced_embeddings).write_image(
-            os.path.join(os.environ['IMAGES_PATH'], 'clusters_2d.pdf'))
+            os.path.join(os.environ['IMAGES_PATH'], f'clusters_2d.{ext}'))
+    topic_model.visualize_heatmap().write_image(
+        os.path.join(os.environ['IMAGES_PATH'],
+                     f'topics_similarity_matrix.{ext}'))
 
 
-def save_topics(kwds: list, topic_model: BERTopic) -> None:
+def save_topics_info(kwds: list, topic_model: BERTopic) -> None:
     """
-    The save_topics function saves the topic model, a json file containing the topics,
-    a csv file containing information about each document's topic distribution and a csv
-    file containing information about each topic. The function takes two arguments:
+    The save_topics_info
+ function saves the topic model, a json file containing the topics,
+    a csv file containing inextion about each document's topic distribution and a csv
+    file containing inextion about each topic. The function takes two arguments:
     kwds (list) - A list of keywords to be used in the BERTopic model.
     topic_model (BERTopic) - A BERTopic object.
 
@@ -81,20 +88,27 @@ def save_topics(kwds: list, topic_model: BERTopic) -> None:
     -------
         None
     """
-    topic_model.save(os.path.join(os.environ['MODELS_PATH'], 'topic_model'))
-    with open(os.path.join(os.environ['MODELS_PATH'], 'bertopics.json'),
+    topic_model.save(os.path.join(os.environ['TOPICS_PATH'], 'topic_model'))
+    with open(os.path.join(os.environ['TOPICS_PATH'], 'top_words.json'),
               'w') as f:
         json.dump(topic_model.get_topics(), f)
-    topic_model.get_document_info(kwds).to_csv(os.path.join(
-        os.environ['INTERIM_DATA_PATH'], 'all_kwds.csv'),
-                                               index=None)
-    topic_model.get_topic_info().to_csv(os.path.join(
-        os.environ['INTERIM_DATA_PATH'], 'topics_info.csv'),
-                                        index=None)
+    with open(os.path.join(os.environ['TOPICS_PATH'], 'topic_labels.json'),
+              'w') as f:
+        json.dump(topic_model.topic_labels_, f)
+    with open(os.path.join(os.environ['TOPICS_PATH'], 'repr_kwds.json'),
+              'w') as f:
+        json.dump(topic_model.representative_docs_, f)
+    topic_model.get_document_info(kwds).to_csv(
+        os.path.join(os.environ['INTERIM_DATA_PATH'], 'all_kwds.csv'))
+    topic_model.get_topic_info().to_csv(
+        os.path.join(os.environ['INTERIM_DATA_PATH'], 'topics_info.csv'))
     topic_model.get_document_info(kwds).loc[:, ['Document', 'Name']].groupby(
-        by='Name').agg(lambda x: ' '.join(x)).to_csv(os.path.join(
-            os.environ['INTERIM_DATA_PATH'], 'topics.csv'),
-                                                     index=None)
+        by='Name').agg(lambda x: ' '.join(x)).to_csv(
+            os.path.join(os.environ['INTERIM_DATA_PATH'], 'topics.csv'))
+
+
+def save_data_per_topic():
+    pass
 
 
 def extract_topics(interim_data_file: str) -> None:
@@ -139,8 +153,53 @@ def extract_topics(interim_data_file: str) -> None:
         ctfidf_model=ctfidf_model,  # Step 5 - Extract topic words
     )
     topics, probs = topic_model.fit_transform(kwds)
-    save_topics(kwds, topic_model)
+    save_topics_info(kwds, topic_model)
     visualize_topics(kwds, topic_model)
+    kwds_df = topic_model.get_document_info(kwds)
+    interim_df['Topic'] = interim_df['Criteria'].map(
+        lambda x: kwds_df.loc[kwds_df['Document'] == x, 'Topic'].item())
+    interim_df['Topic_name'] = interim_df['Criteria'].map(
+        lambda x: kwds_df.loc[kwds_df['Document'] == x, 'Name'].item())
+    save_processed_data(interim_df, topic_model)
+
+
+def save_processed_data(interim_df, topic_model):
+    """
+    The save_processed_data function takes the interim dataframe and topic model as inputs.
+    It then creates a copy of the interim dataframe, loads in the forecast features and target from JSON files,
+    and appends them to a list called features_date. It then removes all files from PROCESSED_DATA_PATH using os.system().
+    Next it loops through each topic in topic model's keys (topic labels) and creates a new dictionary entry for each one with its name as key.
+    The value is set to be processed df filtered by that specific topic label, grouped by date summing up all values for that day across
+
+    Parameters
+    ----------
+        interim_df
+            Pass the dataframe containing the interim data
+        topic_model
+            Identify the topic labels
+
+    Returns
+    -------
+        None
+    """
+    processed_df = interim_df.copy(deep=True)
+    with open(
+            os.path.join(os.environ['FEATURES_PATH'],
+                         'forecast_features.json'), 'r') as f:
+        features = json.load(f)
+    with open(os.path.join(os.environ['FEATURES_PATH'], 'target.json'),
+              'r') as f:
+        target = json.load(f)
+    features_date = features + ['Date', target]
+    os.system(f'rm -rf {os.environ["PROCESSED_DATA_PATH"]}/*')
+    forecast_data = {}
+    for topic in topic_model.topic_labels_.keys():
+        forecast_data[topic] = processed_df.loc[processed_df['Topic'] == topic,
+                                                features_date].groupby(
+                                                    by='Date').sum()
+        forecast_data[topic].to_csv(
+            os.path.join(os.environ['PROCESSED_DATA_PATH'],
+                         f'processed_{topic}.csv'))
 
 
 if __name__ == '__main__':
